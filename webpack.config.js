d9ac91bf401a7a8a2aae947bc82ebc9c8c84e646
Chelsey Denton <cdenton@athenahealth.com>
2017-11-10 17:04:15 -0600

    fix hello world bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: '.app/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js'
-	},
+	entry: './app/index.js',
 	module: {
 		rules: [
 			{ test: /\.(js)$/, use: 'babel-loader' },
 			{ test: /\.css$/, use: ['style-loader', 'css-loader']}
 		]
+	},	
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js'
 	},
 	plugins: [new HtmlWebpackPlugin({ 
 		template: 'app/index.html' 
