f5bc64b6a9951c2c0dc46ec26e1f7029fc5e70ca
Chelsey Denton <cdenton@athenahealth.com>
2017-11-11 16:32:22 -0600

    repos grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props) {
 	var getStyle = (lang, selected) => {
@@ -26,28 +27,81 @@ SelectLanguage.propTypes = {
 	onSelect: PropTypes.func.isRequired
 }
 
+function Repos(props){
+	return (
+		<ul className='popular-list'>
+		{props.repos.map((repo, index) => {
+			return (
+			<li key={repo.name} className='popular-item'>
+				<div className='popular-rank'>#{index+1}</div>
+				<ul className='space-list-items'>
+					<li>
+						<img className='avatar'
+							src={repo.owner.avatar_url} 
+							alt={'Avatar for '+repo.owner.login}/>
+					</li>
+					<li><a href={repo.html_url}>{repo.name}</a></li>
+					<li>@{repo.owner.login}</li>
+					<li>{repo.stargazers_count} stars</li>
+				</ul>
+			</li>
+			);
+		})}
+		</ul>
+	)
+}
+Repos.propTypes = {
+	repos: PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		html_url: PropTypes.string.isRequired,
+		stargazers_count: PropTypes.number.isRequired,
+		owner: PropTypes.shape({
+			avatar_url: PropTypes.string.isRequired,
+			login: PropTypes.string.isRequired,
+		}).isRequired
+	})).isRequired
+}
+
 class Popular extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 		
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
 	
+	componentDidMount() {
+		this.updateLanguage(this.state.selectedLanguage);
+	}
+	
 	updateLanguage(lang){
 		this.setState(function(){
 			return {
-				selectedLanguage: lang
+				selectedLanguage: lang,
+				repos: null
 			}
 		});
+		
+		api.fetchPopularRepos(lang).then(function(repos){
+			this.setState(function(){
+				return {
+					repos: repos
+				}
+			});
+		}.bind(this));
 	}
 	
 	render () {
 		return (
 			<div>
 				<SelectLanguage selected={this.state.selectedLanguage} onSelect={this.updateLanguage} />
+				{ !this.state.repos ? 
+					(<div>Loading...</div>)
+					: (<Repos repos={this.state.repos} />)
+				}
 			</div>
 		);
 	}
