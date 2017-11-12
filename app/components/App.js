3c6fee77ad8d6dbe99fe88ec1999d61210f97489
Chelsey Denton <cdenton@athenahealth.com>
2017-11-11 21:45:18 -0600

    home

--- app/components/App.js
+++ app/components/App.js
@@ -1,21 +1,11 @@
 var React = require('react');
 var Popular = require('./Popular');
 var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
-
-const Home = () => (
-  <div>
-	<h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div>
-	<h2>About</h2>
-  </div>
-)
 
 class App extends React.Component {	
 	render () {
@@ -24,7 +14,7 @@ class App extends React.Component {
 				<div className='container'>
 					<Nav />
 					<Route exact path="/" component={Home}/>
-					<Route path="/battle" component={About}/>
+					<Route path="/battle" component={Battle}/>
 					<Route path="/popular" component={Popular}/>
 				</div>
 			</Router>
---
+++ app/components/Battle.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+class Battle extends React.Component {	
+	render () {
+		return (
+			<div>
+				Battle
+			</div>
+		);
+	}
+}
+
+module.exports = Battle;
